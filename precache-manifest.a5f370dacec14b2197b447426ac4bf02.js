191e5d8bf7063d67b391d4e5748f9b347d19ee82
Paul Newby <paulnewby@Pauls-iMac.home>
2020-08-04 17:20:46 +0100

    Updates

---
+++ precache-manifest.a5f370dacec14b2197b447426ac4bf02.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0e42fa84019756b310c0e258022a374",
+    "url": "/Pomodoro-Clock/index.html"
+  },
+  {
+    "revision": "4a810929fab5f8d5ff27",
+    "url": "/Pomodoro-Clock/static/css/main.58030fc5.chunk.css"
+  },
+  {
+    "revision": "6a2bf3daf33e4e87425e",
+    "url": "/Pomodoro-Clock/static/js/2.d08d9247.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Pomodoro-Clock/static/js/2.d08d9247.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a810929fab5f8d5ff27",
+    "url": "/Pomodoro-Clock/static/js/main.28aeb06f.chunk.js"
+  },
+  {
+    "revision": "7858ea67ab1b0bf1d708",
+    "url": "/Pomodoro-Clock/static/js/runtime-main.a12b2856.js"
+  }
+]);
